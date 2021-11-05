15222c42cd4950cd8b129547e332545d88bb94db
luirusso <luirusso_@hotmail.it>
2021-11-05 16:01:17 +0100

    condensed everything into a function

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es: http
 Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
  */
 
+/**
+ * Steps necessari:
+ * 1. Creazione array con contenuto dei post.
+ * 2. Creare un ciclo for per iterare l'array e creare i post.
+ * 3. Inserire valori dinamici all'interno del ciclo per creare automaticamente i post.
+ * 4. Provare a creare una funziona per automatizzare il processo e renderlo facilmente riproducibile.
+ */
+
+
 const posts = [
     {
         profilePic: 'https://unsplash.it/300/300?image=15',
@@ -55,39 +64,49 @@ console.table(posts);
 const postsContainer = document.querySelector('.posts-list');
 console.log(postsContainer);
 
-for(let i = 0; i < posts.length; i++) {
-    const postItem = posts[i];
+genPosts(postsContainer, posts);
 
-    postsContainer.innerHTML += `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${postItem.profilePic}" alt="${postItem.profileName}">                    
+/**
+ * Generate posts function
+ */
+
+function genPosts(domContainer, array) {
+    domContainer.innerHTML = '';
+
+    for(let i = 0; i < array.length; i++) {
+        const data = array[i];
+    
+        domContainer.innerHTML += `
+
+        <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${data.profilePic}" alt="${data.profileName}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${data.profileName}</div>
+                            <div class="post-meta__time">${data.date}</div>
+                        </div>                    
                     </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${postItem.profileName}</div>
-                        <div class="post-meta__time">${postItem.date}</div>
-                    </div>                    
                 </div>
-            </div>
-            <div class="post__text">${postItem.postText}</div>
-            <div class="post__image">
-                <img src="${postItem.picture}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${postItem.likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-    </div>`;
+                <div class="post__text">${data.postText}</div>
+                <div class="post__image">
+                    <img src="${data.picture}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${data.likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+        </div>`;
+    }
 }
-
