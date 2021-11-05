338b667c4057ebe8413d8bf692ea998c43f6666d
luirusso <luirusso_@hotmail.it>
2021-11-05 15:14:12 +0100

    project setup

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Ricreiamo un feed social aggiungendo al layout di base fornito (allegato sotto) il nostro codice javascript in cui:
+- Creiamo il nostro array di 5 oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine, numero di likes.
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es: https://picsum.photos/id/237/600/350
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli step richiesti di logica in italiano, per aiutarvi a ragionare prima della stesura del codice.
+ */
+
